6c2572fc6c7e7942f0a14c74dc75816055e32d75
Harsh Singh <harsh.singh.2003dydt@gmail.com>
2025-11-27 23:33:59 +0530

    Add input and output files for recursion programs; implement isSorted function

---
+++ reccursion_sorted.cpp
@@ -0,0 +1,30 @@
+// Given an array, find whether it is sorted or not using reccursion
+//i/p: {},{10} o/p: true
+//i/p:{10,1,2,6,9,5} o/p:false
+
+#include <iostream>
+
+using namespace std;
+
+bool isSorted(int arr[], int size){
+    if(size==0||size==1) return true;
+    if(arr[0]>arr[1]) return false;
+    return isSorted(arr+1, size-1);
+}
+
+int main(){
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+     int arr[1000];
+     int n;
+     cin>>n;
+     //Taking input for array
+     for(int i=0; i<n; i++){
+        cin>>arr[i];
+     }
+     cout<<(isSorted(arr,n))?"true":"false";
+     return 0;
+}
