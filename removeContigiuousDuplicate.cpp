64c03fa02aa1b3ff29c51fece50598ff4d9d97a9
Harsh Singh <harsh.singh.2003dydt@gmail.com>
2025-11-29 22:35:32 +0530

    Add removeContagiousDuplicates function to eliminate contiguous duplicate characters in a string

---
+++ removeContigiuousDuplicate.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+void removeContagiousDuplicates(string &str, size_t idx=0){
+	if(idx+1>str.size()) return;
+   //if the first and second char matches
+	if(str[idx]==str[idx+1]){
+		str.erase(idx,1);
+		return removeContagiousDuplicates(str,idx);
+	}
+   //no match
+	return removeContagiousDuplicates(str,idx+1);
+}
+int main(){
+
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+	string s;
+	
+	getline(cin,s);
+	removeContagiousDuplicates(s);
+	cout<<s<<" ";
+	return 0;
+}
