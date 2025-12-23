788fda1c3fb65e2a7ce82d899865960b67697551
Harsh Singh <harsh.singh.2003dydt@gmail.com>
2025-12-24 00:50:08 +0530

    implemented moving 0s to end leetcode question

---
+++ array/movezeros.cpp
@@ -0,0 +1,24 @@
+#include <vector>
+using namespace std;
+class Solution {
+public:
+    vector<int> solve(vector<int>&nums){
+        int n=nums.size();
+        int k=0;//slow pointer
+        for(int i=0;i<n;i++){
+            if(nums[i]!=0){
+                nums[k]=nums[i];
+                k++;
+            }
+        }
+        //pad the remaining with 0s
+        while(k!=n){
+            nums[k]=0;
+            k++;
+        }
+        return nums;
+    }
+    void moveZeroes(vector<int>& nums) {
+        solve(nums);
+    }
+};
