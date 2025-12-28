684d8f95af6ebb1184ef67254d2fb90a554cee9e
Harsh Singh <harsh.singh.2003dydt@gmail.com>
2025-12-28 22:41:17 +0530

    added brute force implementation of leaders in array

---
+++ array/leaderarray.cpp
@@ -0,0 +1,23 @@
+//brute force approach
+#include <iostream>
+#include<vector>
+using namespace std;
+int main()
+{
+    //O(n^2)
+    int n;
+    cin>>n;
+    vector<int>arr(n);
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    for(int i=0;i<n;i++){
+        int j;
+        for(j=i+1;j<n;j++){
+            if(arr[j]>arr[i])//not leader
+            break;
+        }
+        if(j==n) cout<<arr[i]<<" ";
+    }
+    return 0;
+}
