2982dbd60ba7baae1edc63e997706cfb80363ac7
Harsh Singh <harsh.singh.2003dydt@gmail.com>
2026-01-31 22:55:22 +0530

    solved leetcode 485

---
+++ array/maxconsecutiveones.cpp
@@ -0,0 +1,20 @@
+#include<vector>
+using namespace std;
+class Solution {
+public:
+    int findMaxConsecutiveOnes(vector<int>& nums) {
+        int n=nums.size();
+        int maxcount=0;
+        int count=0;
+        for(int i=0;i<n;i++){
+            if(nums[i]==1){
+               count++;
+               maxcount=max(count,maxcount);
+            }else{
+                count=0;
+            }
+        }
+        return maxcount;
+        
+    }
+};
