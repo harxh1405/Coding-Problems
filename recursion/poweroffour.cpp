94a3cea9e7eaaad87adae9529a596e81c67006a2
Harsh Singh <harsh.singh.2003dydt@gmail.com>
2026-02-01 23:48:44 +0530

    solved leetcode 342

---
+++ recursion/poweroffour.cpp
@@ -0,0 +1,11 @@
+
+class Solution {
+public:
+    bool isPowerOfFour(int n) {
+        if(n==1) return true;
+
+        else if(n%4!=0||n<=0) return false;
+
+        else return isPowerOfFour(n/4);
+    }
+};
