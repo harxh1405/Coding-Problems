a23ddd084146842cfc34e40ed3d5ba864bb623fc
Harsh Singh <harsh.singh.2003dydt@gmail.com>
2026-02-04 23:29:29 +0530

    implemented leetcode 162

---
+++ binary search/findpeakelement.cpp
@@ -0,0 +1,17 @@
+#include<vector>
+using namespace std;
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {
+        int n=nums.size();
+        int l=0;
+        int r=n-1;
+        while(l<r){
+            int mid=l+(r-l)/2;
+            if(mid<n-1 && nums[mid+1]>nums[mid]) l=mid+1;
+            else if(mid>0 && nums[mid-1]>nums[mid]) r=mid-1;
+            else return mid;
+        }
+        return l;
+    }
+};
