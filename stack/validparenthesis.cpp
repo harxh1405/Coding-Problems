5e0700ec3bc9acdac12a7730a620872b25e02d5b
Harsh Singh <harsh.singh.2003dydt@gmail.com>
2026-02-05 23:04:38 +0530

    solved valid parentheses problem using stack

---
+++ stack/validparenthesis.cpp
@@ -0,0 +1,21 @@
+#include<string>
+#include<stack>
+using namespace std;
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char>st;
+        for(auto c:s){
+            if(c=='('||c=='{'||c=='[') st.push(c);
+            else{
+                if(st.empty()) return false;
+                auto top=st.top();
+                if((c==')' && top!='(')||
+                (c=='}' && top!='{') || 
+                (c==']' && top!='['))return false;
+                st.pop();
+            }
+        }
+        return st.empty();
+    }
+};
