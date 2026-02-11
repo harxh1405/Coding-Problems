51b1738319070c6a01789ce9f3e89ec3215a4fe3
Harsh Singh <harsh.singh.2003dydt@gmail.com>
2026-02-11 23:04:28 +0530

    solved leetcode 1415

---
+++  recursion and backtracking/leetcode1415.cpp
@@ -0,0 +1,28 @@
+#include<string>
+#include<vector>
+using namespace std;
+class Solution {
+public:
+    string s="";
+    vector<string>outSet;
+    void dfs(int n){
+       if(n==0){
+        outSet.push_back(s);
+        return;
+       }
+       for(int i=0;i<3;i++){
+        char c='a'+i;
+        if(s.empty()||s.back()!=c){
+            s.push_back(c);
+            dfs(n-1);
+            s.pop_back();
+        }
+        
+    }
+}
+    string getHappyString(int n, int k) {
+        dfs(n);
+        if (k > outSet.size()) return "";
+        return outSet[k-1];
+    }
+};
