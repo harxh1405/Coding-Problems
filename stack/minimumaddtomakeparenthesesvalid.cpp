bdab3fbdc50a347fc16717f83b14500795c49bb0
harxh1405 <harsh.singh.2003dydt@gmail.com>
2026-02-16 23:48:21 +0530

    solved minimum add to make parentheses valid

---
+++ stack/minimumaddtomakeparenthesesvalid.cpp
@@ -0,0 +1,16 @@
+#include <stack>
+#include <string>
+using namespace std;
+class Solution {
+public:
+    int minAddToMakeValid(string s) {
+        stack<char>st;
+
+        for(auto c:s){
+           if(st.empty()) st.push(c);
+           else if(c==')' && st.top()=='(') st.pop();
+           else st.push(c);
+        }
+        return st.size();
+    }
+};
