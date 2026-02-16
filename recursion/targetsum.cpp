3461d63db696a32fcdef146f960a676667b7c233
harxh1405 <harsh.singh.2003dydt@gmail.com>
2026-02-16 23:46:35 +0530

    solved target sum leetcode

---
+++ recursion/targetsum.cpp
@@ -0,0 +1,17 @@
+#include <vector>
+using namespace std;
+class Solution {
+    
+public:
+    int solve(vector<int>&nums,int target,int sum,int i){
+        if(i==nums.size()){
+            if(sum==target) return 1;
+            else return 0;
+        }
+        return (solve(nums,target,sum+nums[i],i+1)+solve(nums,target,sum-nums[i],i+1));
+    }
+
+    int findTargetSumWays(vector<int>& nums, int target) {
+        return solve(nums,target,0,0);
+    }
+};
