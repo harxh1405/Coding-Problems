2092ce1e063090a384013cf0bd9f5045887012b0
harxh1405 <harsh.singh.2003dydt@gmail.com>
2026-03-08 23:59:32 +0530

    solved grouping anagrams on leetcode

---
+++ hashing/groupanagrams.cpp
@@ -0,0 +1,24 @@
+#include <vector>
+#include <string>
+#include <unordered_map>
+#include <algorithm>
+using namespace std;
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string,vector<string>>mp;
+        for(auto &word:strs){
+          string key=word;
+          sort(key.begin(),key.end());
+          mp[key].push_back(word);
+        }
+
+        vector<vector<string>> result;
+
+        for(auto &pair:mp){
+            result.push_back(pair.second);
+        }
+
+        return result;
+    }
+};
