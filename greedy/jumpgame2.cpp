34a9a86c796d9f3adc7bc4d78eb28244a88a7ebc
harxh1405 <harsh.singh.2003dydt@gmail.com>
2026-03-10 23:45:04 +0530

    solved leetcode 45

---
+++ greedy/jumpgame2.cpp
@@ -0,0 +1,20 @@
+#include <vector>
+using namespace std;
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        int n=nums.size();
+        int l=0,r=0,count=0;
+        while(r<n-1){
+            int farthest=0;
+            for(int i=l;i<=r;i++){
+                 farthest=max(farthest,nums[i]+i);
+            }
+            l=r+1;
+            r=farthest;
+            count++;
+        }
+
+        return count;
+    }
+};
