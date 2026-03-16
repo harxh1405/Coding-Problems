23651ffdd380f0c8a01b2b05f7bb4d571e30352a
harxh1405 <harsh.singh.2003dydt@gmail.com>
2026-03-16 23:53:41 +0530

    updated leetcode 739

--- stack/dailytemperatures.cpp
+++ stack/dailytemperatures.cpp
@@ -4,16 +4,18 @@ using namespace std;
 class Solution {
 public:
     vector<int> dailyTemperatures(vector<int>& temperatures) {
-        int n=temperatures.size();
         stack<int>st;
+        int n=temperatures.size();
         vector<int>ans(n,0);
+
         for(int i=0;i<n;i++){
-            while(!st.empty() && temperatures[st.top()]<temperatures[i]){
-               ans[st.top()]=i-st.top();
-               st.pop();
+            while(!st.empty() && temperatures[i]>temperatures[st.top()]){
+                ans[st.top()]=i-st.top();
+                st.pop();
             }
             st.push(i);
         }
-        return ans; 
+
+        return ans;
     }
 };
