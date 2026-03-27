c2de4364954383e067f0f4b1aa097b283c54e3c1
harxh1405 <harsh.singh.2003dydt@gmail.com>
2026-03-27 23:45:18 +0530

    solved leetcode 435

---
+++ greedy/nonoverlappingintervals.cpp
@@ -0,0 +1,22 @@
+#include <vector>
+#include <algorithm>
+using namespace std;
+class Solution {
+public:
+    int eraseOverlapIntervals(vector<vector<int>>& intervals) {
+        int n=intervals.size();
+
+        sort(intervals.begin(),intervals.end(),[](vector<int> &a, vector<int> &b){
+             return a[1]<b[1];
+        });
+
+        int count=0;
+        int prevEnd=intervals[0][1];
+        for(int i=1;i<n;i++){
+             if(intervals[i][0]>=prevEnd) prevEnd=intervals[i][1];
+             else count++;
+        }
+
+        return count;
+    }
+};
