0f329f4bcadabe326b09c913b47782a560e25237
harxh1405 <harsh.singh.2003dydt@gmail.com>
2026-03-31 23:34:29 +0530

    revising fibonacci

--- recursion/fibonacci.cpp
+++ recursion/fibonacci.cpp
@@ -17,3 +17,14 @@ public:
         
     }
 };
+int fib(int n){
+    if(n==0) return 0;
+
+    else if(n==1) return 1;
+
+    int fib1=fib(n-1);
+    int fib2=fib(n-2);
+
+    return fib1+fib2;
+}
+
