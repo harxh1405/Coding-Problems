5ff587f1eab1e946cd48e08d719409340bfe3885
harxh1405 <harsh.singh.2003dydt@gmail.com>
2026-04-07 00:00:58 +0530

    implemented prime algorithm

---
+++ Number Theory/prime.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+class Solution {
+  public:
+    bool isPrime(int n) {
+        // code here
+        if(n==1) return false;
+        for(int i=2;i<=sqrt(n);i++){
+            if(n%i==0) return false;
+        }
+        return true;
+    }
+};
