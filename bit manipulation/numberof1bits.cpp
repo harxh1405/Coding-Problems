92edd6277eb0a586fb7ddbcfeab3042779d38752
harxh1405 <harsh.singh.2003dydt@gmail.com>
2026-04-15 23:32:14 +0530

    solved leetcode 191

---
+++ bit manipulation/numberof1bits.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+class Solution {
+public:
+    int hammingWeight(int n) {
+        int count=0;
+        while(n>0){
+            n=n&n-1;
+            count++;
+        }
+        return count;
+    }
+};
